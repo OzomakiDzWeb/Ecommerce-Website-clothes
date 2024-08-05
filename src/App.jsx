bc6cdc8c579ad3938d7a22740cd937df79426797
OzomakiDzWeb <ayoubbouslama1994@gmail.com>
2024-08-05 06:37:30 +0100

    product section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './component/Navbar'
 import Hero from './component/Hero'
-
+import Products from './component/Products'
+import AOS from "aos";
+import 'aos/dist/aos.css'
 const App = () => {
+  useEffect(()=>{
+    AOS.init({
+      offset:100,
+      duration:800,
+      easing:"ease-in-out-sine",
+      delay:100
+    });
+    AOS.refresh()
+  },[])
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Products/>
     </div>
   )
 }
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -28,18 +28,18 @@ const ImageList = [
 ];
 
 const Hero = () => {
-   var settings = {
-     dots: true,
-     arrows:false,
-     infinite: true,
-     speed: 800,
-     slidesToScroll: 1,
-     autoplay:true,
-     autoplaySpeed:4000,
-     cssEase:'ease-in-out',
-     pauseonHover:false,
-     pauseOnFocus:true
-   };
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseonHover: false,
+    pauseOnFocus: true,
+  };
   return (
     <div
       className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center
@@ -55,13 +55,27 @@ const Hero = () => {
               <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/* text content */}
                 <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order- z-10">
-                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
-                   {itm.title}
+                  <h1
+                    data-aos="zoom-out"
+                    data-aos-duration="500"
+                    data-aos-once="true"
+                    className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                  >
+                    {itm.title}
                   </h1>
-                  <p className="text-sm">
+                  <p
+                    data-aos="zoom-out"
+                    data-aos-duration="500"
+                    data-aos-delay="100"
+                    className="text-sm"
+                  >
                     {itm.description}
                   </p>
-                  <div>
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="300"
+                  >
                     <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
                       Order New
                     </button>
@@ -71,6 +85,8 @@ const Hero = () => {
                 <div className="order-1 sm:order-2 ">
                   <div className="relative z-10">
                     <img
+                      data-aos="zoom-in"
+                      data-aos-once="true"
                       src={itm.img}
                       alt="img1"
                       className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] lg:scale-100  sm:scale-125 object-contain mx-auto"
