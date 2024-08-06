a4264214e5ee5fb8aece1e0c26a0af5c74117405
OzomakiDzWeb <ayoubbouslama1994@gmail.com>
2024-08-06 17:48:30 +0100

    fin project and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import React, { useEffect } from 'react'
-import Navbar from './component/Navbar'
-import Hero from './component/Hero'
-import Products from './component/Products'
+import React, { useEffect } from "react";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import Products from "./component/Products";
 import AOS from "aos";
-import 'aos/dist/aos.css'
-import TopProduct from './component/TopProduct';
-import Banner from './component/Banner';
+import "aos/dist/aos.css";
+import TopProduct from "./component/TopProduct";
+import Banner from "./component/Banner";
+import Subscribe from "./component/Subscribe";
+import Testimonials from "./component/Testimonials";
+import Footer from "./component/Footer";
+import Popup from "./component/Popup";
 const App = () => {
-  useEffect(()=>{
+   const [orderPopup, setOrderPopup] = React.useState(false);
+
+   const handleOrderPopup = () => {
+     setOrderPopup(!orderPopup);
+   };
+  useEffect(() => {
     AOS.init({
-      offset:100,
-      duration:800,
-      easing:"ease-in-out-sine",
-      delay:100
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-out-sine",
+      delay: 100,
     });
-    AOS.refresh()
-  },[])
+    AOS.refresh();
+  }, []);
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Products/>
-      <TopProduct/>
-      <Banner/>
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProduct />
+      <Banner />
+      <Subscribe />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
